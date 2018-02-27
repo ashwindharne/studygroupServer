e6fa627a2e5630363577763d09f676fc145ecb56
gpawlowsky <gabriel_pawlowsky@yahoo.de>
2018-02-26 17:36:37 -0800

    Added basic JSON parsing and packet handling functionality

--- StudyGroupServer/src/User.java
+++ StudyGroupServer/src/User.java
@@ -1,28 +1,41 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class User {
     private double latitude;
     private double longitude;
     private String id;
 
+    public User(String auth_id){
+        id=auth_id;
+    }
+    
     public User(String auth_id, double lat, double lon){
         id=auth_id;
         latitude=lat;
         longitude=lon;
     }//constructor, if necessary give it an ID and initial lat/lon.
+    
     public String getId(){//getters and setters
         return id;
     }
+    
     public double getLatitude(){
         return latitude;
     }
+    
     public double getLongitude(){
         return longitude;
     }
+    
     public void setLatitude(double new_lat){
         latitude=new_lat;
     }
+    
     public void setLongitude(double new_long){
         longitude=new_long;
     }
+    
     public double getDistance(User other){//gets the distance between this user and another user, using lat&long
         double R = 6371;
         double lat_diff=deg2rad(latitude-other.getLatitude());
@@ -33,7 +46,15 @@ public class User {
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         return R*c;
     }
+    
     private double deg2rad(double deg){
         return deg*(Math.PI/180);
+    }
+    
+    public String toJsonString(){
+    	GsonBuilder builder = new GsonBuilder();
+		Gson gson = builder.create();
+		
+    	return gson.toJson(this);
     }
 }
